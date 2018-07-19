8fa5d9d155142d77b198bef205efebc57f6c1bb8
AlexPichGH <luctator310@gmail.com>
2018-07-19 12:55:09 +0300

    скорректирована константа record_msg

--- app/src/main/java/application/p/alex/julyworkout/utils/Constants.java
+++ app/src/main/java/application/p/alex/julyworkout/utils/Constants.java
@@ -2,5 +2,5 @@ package application.p.alex.julyworkout.utils;
 
 public interface Constants {
     String WORKOUT_INDEX = "WORKOUT_INDEX";
-    String RECORD_MSG = "Мой новый рекорд: ";
+    String RECORD_MSG = "Мой рекорд: ";
 }
