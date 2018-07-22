6fe77cf9e13217c3844eab31a254e49a6a4f3208
AlexPichGH <luctator310@gmail.com>
2018-07-22 18:10:24 +0300

    вынес id в Constants, переопределил методы жизненного цикла, добавил логирование

--- app/src/main/java/application/p/alex/julyworkout/WorkoutListActivity.java
+++ app/src/main/java/application/p/alex/julyworkout/WorkoutListActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,9 +16,7 @@ import application.p.alex.julyworkout.model.WorkoutList;
 import application.p.alex.julyworkout.utils.Constants;
 
 public class WorkoutListActivity extends AppCompatActivity {
-    private static final int PULL_UPS_ID = 0;
-    private static final int PUSH_UPS_ID = 1;
-    private static final int SIT_UPS_ID = 2;
+    private static final String TAG = "WorkoutListActivity";
     private List<Workout> workoutList;
     private Button buttonPullUps;
     private Button buttonPushUps;
@@ -31,13 +30,13 @@ public class WorkoutListActivity extends AppCompatActivity {
         if (data != null && resultCode == RESULT_OK) {
             String result = data.getStringExtra(String.valueOf(R.string.record));
             switch (requestCode) {
-                case PULL_UPS_ID:
+                case Constants.PULL_UPS_ID:
                     textViewPullUpsRecord.setText(result);
                     break;
-                case PUSH_UPS_ID:
+                case Constants.PUSH_UPS_ID:
                     textViewPushUpsRecord.setText(result);
                     break;
-                case SIT_UPS_ID:
+                case Constants.SIT_UPS_ID:
                     textViewSitUpsRecord.setText(result);
                     break;
                 default:
@@ -51,6 +50,7 @@ public class WorkoutListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, String.valueOf(R.string.on_create));
         setContentView(R.layout.activity_workout_list);
 
         workoutList = WorkoutList.getInstance(this).getAllWorkouts();
@@ -65,23 +65,59 @@ public class WorkoutListActivity extends AppCompatActivity {
         buttonPullUps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startWorkoutDetailActivity(PULL_UPS_ID);
+                startWorkoutDetailActivity(Constants.PULL_UPS_ID);
             }
         });
 
         buttonPushUps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startWorkoutDetailActivity(PUSH_UPS_ID);
+                startWorkoutDetailActivity(Constants.PUSH_UPS_ID);
             }
         });
 
         buttonSitUps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startWorkoutDetailActivity(SIT_UPS_ID);
+                startWorkoutDetailActivity(Constants.SIT_UPS_ID);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, String.valueOf(R.string.on_start));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, String.valueOf(R.string.on_resume));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, String.valueOf(R.string.on_pause));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, String.valueOf(R.string.on_stop));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, String.valueOf(R.string.on_destroy));
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, String.valueOf(R.string.on_restart));
     }
 
     private void startWorkoutDetailActivity(int i) {
