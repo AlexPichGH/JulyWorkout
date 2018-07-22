f970a6f9e0ed5cb4df23eabd80c52bcf3bdbc007
AlexPichGH <luctator310@gmail.com>
2018-07-22 18:07:04 +0300

    добавлены константы id упражнений

--- app/src/main/java/application/p/alex/julyworkout/utils/Constants.java
+++ app/src/main/java/application/p/alex/julyworkout/utils/Constants.java
@@ -3,4 +3,7 @@ package application.p.alex.julyworkout.utils;
 public interface Constants {
     String WORKOUT_INDEX = "WORKOUT_INDEX";
     String RECORD_MSG = "Мой рекорд: ";
+    int PULL_UPS_ID = 0;
+    int PUSH_UPS_ID = 1;
+    int SIT_UPS_ID = 2;
 }
