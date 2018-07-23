7985f29b70fc801e846ae2ff677788348d38ef41
AlexPichGH <luctator310@gmail.com>
2018-07-23 14:15:37 +0300

    вынес константы для сохранения рекордов

--- app/src/main/java/application/p/alex/julyworkout/utils/Constants.java
+++ app/src/main/java/application/p/alex/julyworkout/utils/Constants.java
@@ -3,6 +3,9 @@ package application.p.alex.julyworkout.utils;
 public interface Constants {
     String WORKOUT_INDEX = "WORKOUT_INDEX";
     String RECORD_MSG = "Мой рекорд: ";
+    String RECORD_PULL_UP_SAVE = "recordpullupsave";
+    String RECORD_PUSH_UP_SAVE = "recordpushupsave";
+    String RECORD_SIT_UP_SAVE = "recordsitupsave";
     int PULL_UPS_ID = 0;
     int PUSH_UPS_ID = 1;
     int SIT_UPS_ID = 2;
