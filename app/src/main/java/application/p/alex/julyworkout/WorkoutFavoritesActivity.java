7d7ca3a93b740cbe5a3a03f6cf1cb9fa58515158
AlexPichGH <luctator310@gmail.com>
2018-08-01 23:07:48 +0300

    добавил изображения для упражнений, а также активность для избранного

---
+++ app/src/main/java/application/p/alex/julyworkout/WorkoutFavoritesActivity.java
@@ -0,0 +1,13 @@
+package application.p.alex.julyworkout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkoutFavoritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_favorites);
+    }
+}
