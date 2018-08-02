247fd6a188b5c6ed249e9a46e913d3d9b6ebf7ee
AlexPichGH <luctator310@gmail.com>
2018-08-02 16:16:35 +0300

    оформил recycler для избранного

--- app/src/main/java/application/p/alex/julyworkout/WorkoutFavoritesActivity.java
+++ app/src/main/java/application/p/alex/julyworkout/WorkoutFavoritesActivity.java
@@ -1,13 +1,26 @@
 package application.p.alex.julyworkout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import application.p.alex.julyworkout.list.FavoriteWorkoutAdapter;
+import application.p.alex.julyworkout.model.WorkoutList;
 
 public class WorkoutFavoritesActivity extends AppCompatActivity {
+    private RecyclerView workoutRecycler;
+    private FavoriteWorkoutAdapter favoriteWorkoutAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_favorites);
+
+        workoutRecycler = findViewById(R.id.workout_favorites_recycler);
+        favoriteWorkoutAdapter = new FavoriteWorkoutAdapter(this, WorkoutList.getInstance(this).getFavoriteWorkouts());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        workoutRecycler.setLayoutManager(linearLayoutManager);
+        workoutRecycler.setAdapter(favoriteWorkoutAdapter);
     }
 }
