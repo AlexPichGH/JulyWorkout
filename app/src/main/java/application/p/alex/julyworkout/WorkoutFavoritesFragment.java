03479d7a2d05bc95c26cce63d4473c9537d03c40
AlexPichGH <luctator310@gmail.com>
2018-08-06 19:50:49 +0300

    заменил активность WorkoutFavorites на фрагмент

--- app/src/main/java/application/p/alex/julyworkout/WorkoutFavoritesFragment.java
+++ app/src/main/java/application/p/alex/julyworkout/WorkoutFavoritesFragment.java
@@ -1,26 +1,36 @@
 package application.p.alex.julyworkout;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-import application.p.alex.julyworkout.list.FavoriteWorkoutAdapter;
+import application.p.alex.julyworkout.list.WorkoutAdapter;
 import application.p.alex.julyworkout.model.WorkoutList;
 
-public class WorkoutFavoritesActivity extends AppCompatActivity {
+public class WorkoutFavoritesFragment extends Fragment {
     private RecyclerView workoutRecycler;
-    private FavoriteWorkoutAdapter favoriteWorkoutAdapter;
+    private WorkoutAdapter workoutAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_workout_favorites, container, false);
+        workoutRecycler = root.findViewById(R.id.workout_favorites_recycler);
+        workoutAdapter = new WorkoutAdapter(getContext(), WorkoutList.getInstance(getContext()).getFavoriteWorkouts());
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        workoutRecycler.setLayoutManager(linearLayoutManager);
+        workoutRecycler.setAdapter(workoutAdapter);
+        return root;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_workout_favorites);
-
-        workoutRecycler = findViewById(R.id.workout_favorites_recycler);
-        favoriteWorkoutAdapter = new FavoriteWorkoutAdapter(this, WorkoutList.getInstance(this).getFavoriteWorkouts());
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        workoutRecycler.setLayoutManager(linearLayoutManager);
-        workoutRecycler.setAdapter(favoriteWorkoutAdapter);
     }
 }
