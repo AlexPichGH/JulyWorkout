cf3213778a1c62aad3020cc8d899dcc98fe4f421
AlexPichGH <luctator310@gmail.com>
2018-08-09 14:51:54 +0300

    добавил экземпляр интерфейса, исправил реализацию detailFragment

--- app/src/main/java/application/p/alex/julyworkout/list/WorkoutAdapter.java
+++ app/src/main/java/application/p/alex/julyworkout/list/WorkoutAdapter.java
@@ -1,7 +1,6 @@
 package application.p.alex.julyworkout.list;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,14 +11,14 @@ import android.widget.Toast;
 import java.util.List;
 
 import application.p.alex.julyworkout.R;
-import application.p.alex.julyworkout.WorkoutDetailActivity;
+import application.p.alex.julyworkout.interfaces.OnWorkoutListItemSelectedListener;
 import application.p.alex.julyworkout.model.Workout;
 import application.p.alex.julyworkout.model.WorkoutList;
-import application.p.alex.julyworkout.utils.Constants;
 
 public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutViewHolder> {
-    Context context;
-    List<Workout> workouts;
+    private Context context;
+    private List<Workout> workouts;
+    private OnWorkoutListItemSelectedListener itemSelectedListener;
 
     public WorkoutAdapter(Context context, List<Workout> workouts) {
         this.context = context;
@@ -36,26 +35,25 @@ public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull final WorkoutViewHolder workoutViewHolder, final int i) {
         workoutViewHolder.initUI(workouts.get(i));
+        itemSelectedListener = (OnWorkoutListItemSelectedListener) context;
         workoutViewHolder.item.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, WorkoutDetailActivity.class);
-                intent.putExtra(Constants.WORKOUT_INDEX, workoutViewHolder.getAdapterPosition());
-                context.startActivity(intent);
+                itemSelectedListener.onWorkoutListItemSelected(workoutViewHolder.getAdapterPosition());
             }
         });
         workoutViewHolder.favorite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!WorkoutList.getInstance(context).getWorkout(i).isFavorite()) {
+                if (!WorkoutList.getInstance(context).getWorkout(workoutViewHolder.getAdapterPosition()).isFavorite()) {
                     workoutViewHolder.favorite.setImageResource(R.drawable.ic_favorite_on_24dp);
-                    WorkoutList.getInstance(context).getWorkout(i).setFavorite(true);
-                    WorkoutList.getInstance(context).setFavoriteWorkout(i);
+                    WorkoutList.getInstance(context).setFavoriteWorkout(workoutViewHolder.getAdapterPosition());
+                    WorkoutList.getInstance(context).getWorkout(workoutViewHolder.getAdapterPosition()).setFavorite(true);
                     Toast.makeText(context, "Упражнение добавлено в избранное", Toast.LENGTH_SHORT).show();
-                } else if (WorkoutList.getInstance(context).getWorkout(i).isFavorite()) {
+                } else if (WorkoutList.getInstance(context).getWorkout(workoutViewHolder.getAdapterPosition()).isFavorite()) {
                     workoutViewHolder.favorite.setImageResource(R.drawable.ic_favorite_off_24dp);
-                    WorkoutList.getInstance(context).getWorkout(i).setFavorite(false);
-                    WorkoutList.getInstance(context).removeFavoriteWorkout(i);
+                    WorkoutList.getInstance(context).getWorkout(workoutViewHolder.getAdapterPosition()).setFavorite(false);
+                    WorkoutList.getInstance(context).removeFavoriteWorkout(workoutViewHolder.getAdapterPosition());
                     Toast.makeText(context, "Упражнение удалено из избранного", Toast.LENGTH_SHORT).show();
                 }
             }
