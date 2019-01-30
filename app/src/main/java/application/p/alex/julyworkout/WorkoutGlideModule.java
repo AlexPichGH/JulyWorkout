f9f06effb98e42dd42725db517c44f947deaf163
AlexPichGH <luctator310@gmail.com>
2019-01-30 14:26:28 +0300

    add glide module, code fixes

---
+++ app/src/main/java/application/p/alex/julyworkout/WorkoutGlideModule.java
@@ -0,0 +1,8 @@
+package application.p.alex.julyworkout;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class WorkoutGlideModule extends AppGlideModule {
+}
